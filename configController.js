99e883214eccf5d1a61931e7cab5e9aafe38483c
Lior <audiblemaple@gmail.com>
2023-11-12 12:41:13 +0200

    before file locks during updates

--- configController.js
+++ configController.js
@@ -1,36 +1,31 @@
 const fs = require('fs').promises;
 const path = require('path');
+const lockfile = require('proper-lockfile');
 
 const updateConfig = async (req, res) => {
-    console.log(req.body)
     try {
         const { slot, isOn } = req.body;
-        const configDir = 'D:\\smart_home_server';
-        const configPath = path.join(configDir, 'hotspotConfig.json');
 
-        let config = JSON.parse(await fs.readFile(configPath, 'utf8'));
+        const configPath = path.join(__dirname, 'hotspotConfig.json');
+        const config = JSON.parse(await fs.readFile(configPath, 'utf8'));
 
-        // Check if the slot exists in the config and update its 'isOn' value
         if (config.hasOwnProperty(slot)) {
             config[slot].isOn = isOn;
-        } else {
-            // Handle the case where the slot doesn't exist in the config
-            return res.status(404).json({ message: 'Slot not found' });
-        }
-
-        await fs.writeFile(configPath, JSON.stringify(config, null, 2));
-        res.status(200).json({ message: 'Hotspot config updated successfully' });
+            await fs.writeFile(configPath, JSON.stringify(config, null, 2));
+            res.status(200).json({ message: 'Hotspot config updated successfully'});
+        } else
+            res.status(404).json({ message: 'Slot not found' });
     } catch (error) {
         console.error('Error updating the hotspot config:', error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).json({ message: 'Internal Server Error' });
     }
 };
 
 const getConfig = async (req, res) => {
     try {
-        const configPath = path.join('D:\\smart_home_server', 'hotspotConfig.json');
+        const configPath = path.join(__dirname, 'hotspotConfig.json');
         const rawConfig = await fs.readFile(configPath, 'utf8');
-        console.log('Raw config:', rawConfig); // Log the raw file content
+        console.log('Raw config:', rawConfig);
         const config = JSON.parse(rawConfig);
         res.status(200).json(config);
     } catch (error) {
@@ -40,3 +35,48 @@ const getConfig = async (req, res) => {
 };
 
 module.exports = { updateConfig, getConfig };
+
+
+
+
+
+
+
+
+
+// const fs = require('fs').promises;
+// const path = require('path');
+//
+// const updateConfig = async (req, res) => {
+//     try {
+//         const { slot, isOn } = req.body;
+//
+//         const configPath = path.join(__dirname, 'hotspotConfig.json');
+//         const config = JSON.parse(await fs.readFile(configPath, 'utf8'));
+//
+//         if (config.hasOwnProperty(slot)) {
+//             config[slot].isOn = isOn;
+//             await fs.writeFile(configPath, JSON.stringify(config, null, 2));
+//             res.status(200).json({ message: 'Hotspot config updated successfully'});
+//         } else
+//             res.status(404).json({ message: 'Slot not found' });
+//     } catch (error) {
+//         console.error('Error updating the hotspot config:', error);
+//         res.status(500).json({ message: 'Internal Server Error' });
+//     }
+// };
+//
+// const getConfig = async (req, res) => {
+//     try {
+//         const configPath = path.join(__dirname, 'hotspotConfig.json');
+//         const rawConfig = await fs.readFile(configPath, 'utf8');
+//         console.log('Raw config:', rawConfig);
+//         const config = JSON.parse(rawConfig);
+//         res.status(200).json(config);
+//     } catch (error) {
+//         console.error('Error fetching the config file:', error);
+//         res.status(500).send('Internal Server Error');
+//     }
+// };
+//
+// module.exports = { updateConfig, getConfig };
