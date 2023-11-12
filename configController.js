554074404683a7b9cb2abff9fd1591a7ab68bbcb
Lior <audiblemaple@gmail.com>
2023-11-12 12:53:36 +0200

    fixed errors when updating the hotspotbuttons from the config due to multiple simultaneous file accesses

--- configController.js
+++ configController.js
@@ -2,22 +2,40 @@ const fs = require('fs').promises;
 const path = require('path');
 const lockfile = require('proper-lockfile');
 
+const configPath = path.join(__dirname, 'hotspotConfig.json');
+
 const updateConfig = async (req, res) => {
+    let releaseLock;
     try {
+        releaseLock = await lockfile.lock(configPath, {
+            retries: {
+                retries: 6,
+                maxTimeout: 1000,
+            }
+        });
+
+        const rawConfig = await fs.readFile(configPath, 'utf8');
+        const config = JSON.parse(rawConfig);
+
         const { slot, isOn } = req.body;
-
-        const configPath = path.join(__dirname, 'hotspotConfig.json');
-        const config = JSON.parse(await fs.readFile(configPath, 'utf8'));
-
         if (config.hasOwnProperty(slot)) {
             config[slot].isOn = isOn;
             await fs.writeFile(configPath, JSON.stringify(config, null, 2));
-            res.status(200).json({ message: 'Hotspot config updated successfully'});
-        } else
+            res.status(200).json({ message: 'Hotspot config updated successfully' });
+        } else {
             res.status(404).json({ message: 'Slot not found' });
+        }
     } catch (error) {
         console.error('Error updating the hotspot config:', error);
-        res.status(500).json({ message: 'Internal Server Error' });
+        res.status(500).send('Internal Server Error');
+    } finally {
+        if (releaseLock) {
+            try {
+                await releaseLock();
+            } catch (unlockError) {
+                console.error('Error releasing file lock:', unlockError);
+            }
+        }
     }
 };
 
