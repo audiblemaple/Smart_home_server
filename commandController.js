6a99faceef53dc993532e3c3a8d9e45d1e12e4fb
Lior <audiblemaple@gmail.com>
2023-12-16 13:14:51 +0200

    added token handling on the server side and root node ip from the config

--- commandController.js
+++ commandController.js
@@ -3,24 +3,31 @@ let fetch;
 (async () => {
     fetch = (await import('node-fetch')).default;
 })();
+
+// Synchronously read the JSON file
+const configData = fs.readFileSync('serverConfig.json');
+const config = JSON.parse(configData);
+
+const token = config.token;
+const rootIp = config.rootIp;
+
 const sendCommand = async (req, res) => {
     const { nodeID, action, token } = req.body;
 
-    // Construct the new URL
-    const url = `http://192.168.1.115/comm?id=${nodeID}&act=${action}&token=${token}`;
+    const url = `http://${rootIp}/comm?id=${nodeID}&act=${action}&token=${token}`;
 
     // Send the GET request to the new URL
     fetch(url)
         .then(response => response.text())
         .then(result => {
             console.log('Request sent successfully', result);
-            res.status(200).send({ message: 'Command forwarded successfully' });
+            res.status(200).send({ message: 'Command sent successfully' });
         })
         .catch(error => {
-            console.error('Error forwarding command:', error);
+            console.error('Error sending command:', error);
             // You can choose a different status code based on the error type.
             // For this example, I'm using 500 (Internal Server Error).
-            res.status(500).send({ message: 'Error forwarding command' });
+            res.status(500).send({ message: 'Error sending command' });
         });
 };
 
